061e04430b1b1e2a8a2e16a6e419d80b118984b3
battorydev <113938540+battorydev@users.noreply.github.com>
2023-04-27 20:31:23 +0700

    Change http to websocket using gorilla websocket

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"net/http"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool{
+		return true
+	},
+}
 
 func main(){
 	http.HandleFunc("/", handler)
@@ -11,5 +20,26 @@ func main(){
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello World from Go!")
+	//fmt.Fprintf(w, "Hello World from Go!")
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		msgType, msg, err := socket.ReadMessage()
+		if err != nil{
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(string(msg))
+		err = socket.WriteMessage(msgType, msg)
+		if err != nil{
+			fmt.Println(err)
+			return
+		}
+
+	}
 }
