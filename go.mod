c02f2eaa22fd8d075723fcaab41c49d6a7725323
battorydev <113938540+battorydev@users.noreply.github.com>
2023-04-28 22:43:31 +0700

    Use mapstructure to decode json

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.20
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+)
--- junk.go
+++ junk.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"encoding/json"
+	"github.com/mitchellh/mapstructure"
 )
 
 type Message struct {
@@ -33,7 +34,16 @@ func main(){
 	fmt.Printf("%#v\n", recMessage)	// %#v prints out the struct with the field names
 
 	if recMessage.Name == "channel add"{
-		addChannel(recMessage.Data)
+		channel, err := addChannel(recMessage.Data)
+		var sendMessage Message
+		sendMessage.Name = "channel add"
+		sendMessage.Data = channel
+		sendRawMsg, err := json.Marshal(sendMessage)	// returns a byte array
+		if err != nil{
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(sendRawMsg))
 	}
 
 
@@ -44,8 +54,11 @@ func addChannel(data interface{}) (Channel, error) {
 	fmt.Println(data)
 
 	var channel Channel
-	channelMap := data.(map[string]interface{})
-	channel.Name = channelMap["name"].(string)
+	err := mapstructure.Decode(data, &channel)
+	if err != nil{
+		return channel, err
+	}
+
 	channel.Id = "1"
 	fmt.Printf("%#v\n", channel)
 	return channel, nil
